2bc476d7f0da5af7519b7826984251664b6ff454
Arian Sani <ariansani@hotmail.com>
2022-03-20 00:45:55 +0800

    Added

---
+++ src/main/java/vttp2022/nusiss/MockAss2/MockAss2Application.java
@@ -0,0 +1,13 @@
+package vttp2022.nusiss.MockAss2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockAss2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockAss2Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/nusiss/MockAss2/MockAss2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.nusiss.MockAss2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockAss2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
